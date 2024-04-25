2898d1702dd8bda79df609f88f75681c0fe78c9b
Johan  Antonio <147890607+johansitoweb@users.noreply.github.com>
2024-04-25 16:18:00 +0200

    Add files via upload

---
+++ src/components/Mont.svelte
@@ -0,0 +1,20 @@
+<script>
+    let naomp ="Lo ultimo sobre los container";
+</script>
+<main>
+    <!--
+        
+        crea botones y utiliza los contenedores
+
+        los dos tipos de contenedores que pondre .container y container-fluid
+
+        y botones bonitos
+        por lo menos
+        algunos
+        5
+        botones
+    -->
+
+</main>
+
+
